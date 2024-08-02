65642a64643de9d9139c00f2e7f530add1e0c450
DYXO <149788976+DYXO@users.noreply.github.com>
2024-08-02 22:18:03 +0100

    Add files via upload
    add thread

--- inc/ultrasonic.h
+++ inc/ultrasonic.h
@@ -3,23 +3,49 @@
 
 #include <stdio.h>
 #include <pigpio.h>
+#include <thread>  
+
+class UltrasonicCallback {
+public:
+	/*
+	 * Called when receive ehco.
+	 */
+	virtual void distance_ready(float dis) = 0;
+};
 
 class ultrasonic
 {
 public:
 	ultrasonic();
 	~ultrasonic();
-	float echo_wait();
+	
+	/*
+	 * Registers the callback which is called 
+	 * whenever distance is calculated
+	 */
+	void registerCallback(UltrasonicCallback* cb);
+	
+	/*
+	 * Unregisters the callback to the callback interface.
+	 */
+	void unregisterCallback();
+	
+	void start();
+
 private:
+	bool running=0;
 	int seconds;
 	int last_seconds;
 	int micros;
 	int last_micros;
 	int *p_s=&seconds;
 	int *p_m=&micros;
+	std::thread disThread;
+	UltrasonicCallback* callback;
 	float distance;
 	void gpio_init();
 	void wave_start();
+	void echo_wait();
 	const int triggpio=6;
 	const int echogpio=5;
 };
