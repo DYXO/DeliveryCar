d2b913773fc78722f7cdc34ea0c2f773d2b093f5
DYXO <149788976+DYXO@users.noreply.github.com>
2024-08-13 07:32:15 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include "motor.h"
+#include "linetrack.h"
 #include "ultrasonic.h"
+
+bool have_block=0;
+linetrack car;
+void control()
+{	
+	car.track_control(!have_block);
+	gpioSleep(PI_TIME_RELATIVE, 0, 50000); 
+}
+
+class PrintDistanceCallback:public UltrasonicCallback
+{
+	virtual void distance_ready(float d) 
+	{
+		printf("%f\n",d);
+		if(d<0.3)
+		{
+			have_block=1;
+		}
+		else
+		{
+			have_block=0;
+		}
+	}
+};
+
 
 int main(int argc, char *argv[]) 
 {
-	ultrasonic test_ultrasonic;
-	test_ultrasonic.echo_wait();
+	ultrasonic u1;
+	PrintDistanceCallback printdistanceCallback;
+	u1.registerCallback(&printdistanceCallback);
+	u1.start();
+	while(1)
+	{	
+		control();
+	}
 	return 0;
 }
