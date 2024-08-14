d393f451d4ab4bfb31ec344fe9bced4d180f48ea
DYXO <149788976+DYXO@users.noreply.github.com>
2024-08-14 15:20:17 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "motor.h"
 #include "linetrack.h"
 #include "ultrasonic.h"
+#include "playsound.h"
+#include "serial.h"
+#include "cabinet.h"
 
 bool have_block=0;
 linetrack car;
+scanner s1(16);
+cabinet DeliveryBox(&s1);
+soundplayer warning("Sound.wav");
+bool arrive=0;
 void control()
 {	
 	car.track_control(!have_block);
@@ -18,6 +25,7 @@ class PrintDistanceCallback:public UltrasonicCallback
 		if(d<0.3)
 		{
 			have_block=1;
+			std::thread warning_sound(&soundplayer::play,&warning);
 		}
 		else
 		{
@@ -29,13 +37,26 @@ class PrintDistanceCallback:public UltrasonicCallback
 
 int main(int argc, char *argv[]) 
 {
+	while(!gpioInitialise())
+	{
+		printf("erro\n");
+	}
 	ultrasonic u1;
 	PrintDistanceCallback printdistanceCallback;
 	u1.registerCallback(&printdistanceCallback);
 	u1.start();
 	while(1)
 	{	
-		control();
+		DeliveryBox.deposit();
+		if(DeliveryBox.lock==1&&arrive==0)
+		{
+			control();
+		}
+		else if(arrive==1)
+		{
+			DeliveryBox.takeout();
+			break;
+		}
 	}
 	return 0;
 }
